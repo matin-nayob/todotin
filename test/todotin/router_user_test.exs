f866a041ae66c6c3c137b3d3da6d2649058f844d
Matin <m@example.com>
2020-03-28 01:47:49 +0000

    first test

---
+++ test/todotin/router_user_test.exs
@@ -0,0 +1,24 @@
+defmodule Todotin.Routers.UserTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias Todotin.Router.Main
+
+  @opt Main.init([])
+
+  setup do
+    user = Todotin.Model.User.new("user.router@test.com", "User Router Test")
+
+    {:ok, user: user, user_id: user.user_id}
+  end
+
+  test "get_user", context do
+    resp =
+      :get
+      |> conn("/user/#{context[:user_id]}", "")
+      |> Main.call(@opts)
+
+    assert resp.state == :sent
+    assert resp.status == 200
+  end
+end
